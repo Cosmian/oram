9cdac6540cf2ed787d630e72941d0fe8f5e4f6a9
Martin Grenouilloux <martin.grenouilloux@cosmian.com>
2023-08-07 15:11:57 +0200

    [feat] Transformed Node value into an actual bucket of elements
    Created the DataItem element type. Name might not be the best since
    its mostly cipehrtexts. Each DataItem gathers a path and a byte array.

---
+++ README.md
@@ -0,0 +1,9 @@
+# Path-ORAM
+Path-ORAM is an implementation of the algorithm of the same name first anounced in `https://eprint.iacr.org/2013/280.pdf`. It aims at providing a system a searching algorithm on encrypted data with limited leakage.
+
+## Build test, and run
+To build this program, `cargo 1.67.1` is needed.
+
+Simply run `cargo build` followed by `cargo run`.
+
+The tests can be performed using `cargo test`.
--- src/btree.rs
+++ src/btree.rs
@@ -1,3 +1,7 @@
+use cosmian_crypto_core::reexport::rand_core::CryptoRngCore;
+
+use crate::oram::BUCKET_SIZE;
+
 #[derive(Debug, Clone, Default)]
 pub struct BTree {
     root: Option<Box<Node>>,
@@ -7,7 +11,11 @@ pub struct BTree {
 }
 
 impl BTree {
-    pub fn new_empty_complete(nb_blocks: usize, block_size: usize) -> BTree {
+    pub fn new_empty_complete<CSPRNG: CryptoRngCore>(
+        csprng: &mut CSPRNG,
+        nb_blocks: usize,
+        block_size: usize,
+    ) -> BTree {
         let mut height = nb_blocks.ilog2();
         if !nb_blocks.is_power_of_two() {
             height += 1;
@@ -20,21 +28,26 @@ impl BTree {
             block_size,
         };
 
-        let mut root = Node::default();
+        let mut root = Node::new(csprng);
 
-        BTree::complete_tree(&mut root, tree.height, 0);
+        BTree::complete_tree(csprng, &mut root, tree.height, 0);
         tree.root = Some(Box::new(root));
         tree
     }
 
-    fn complete_tree(node: &mut Node, height: u32, level: u32) {
+    fn complete_tree<CSPRNG: CryptoRngCore>(
+        csprng: &mut CSPRNG,
+        node: &mut Node,
+        height: u32,
+        level: u32,
+    ) {
         // -1 is to avoid constructing 1 extra level.
         if level < height - 1 {
-            let mut left = Box::new(Node::new(level + 1));
-            let mut right = Box::new(Node::new(level + 1));
+            let mut left = Box::new(Node::new(csprng));
+            let mut right = Box::new(Node::new(csprng));
 
-            BTree::complete_tree(&mut left, height, level + 1);
-            BTree::complete_tree(&mut right, height, level + 1);
+            BTree::complete_tree(csprng, &mut left, height, level + 1);
+            BTree::complete_tree(csprng, &mut right, height, level + 1);
 
             node.left = Some(left);
             node.right = Some(right);
@@ -58,19 +71,27 @@ impl BTree {
     }
 }
 
+/* Bucket ciphertexts are not size bounded. Might want to fix this later. */
 #[derive(Debug, Clone, Default)]
 pub struct Node {
     left: Option<Box<Node>>,
     right: Option<Box<Node>>,
-    value: u32, // change to tuple (value, path) later.
+    bucket: [DataItem; BUCKET_SIZE],
 }
 
 impl Node {
-    fn new(value: u32) -> Node {
+    fn new<CSPRNG: CryptoRngCore>(csprng: &mut CSPRNG) -> Node {
+        let bucket = [
+            DataItem::new_random(csprng),
+            DataItem::new_random(csprng),
+            DataItem::new_random(csprng),
+            DataItem::new_random(csprng),
+        ];
+
         Node {
             left: Option::None,
             right: Option::None,
-            value,
+            bucket,
         }
     }
 
@@ -82,7 +103,42 @@ impl Node {
         self.right.as_ref()
     }
 
-    pub fn value(&self) -> u32 {
-        self.value
+    pub fn bucket(&self) -> &[DataItem; BUCKET_SIZE] {
+        &self.bucket
+    }
+}
+
+#[derive(Debug, Clone, Default)]
+pub struct DataItem {
+    data: Vec<u8>,
+    path: u16,
+}
+
+impl DataItem {
+    fn new(data: Vec<u8>, path: u16) -> DataItem {
+        DataItem { data, path }
+    }
+
+    fn new_random<CSPRNG: CryptoRngCore>(csprng: &mut CSPRNG) -> DataItem {
+        let mut data = vec![0; 16];
+        csprng.fill_bytes(&mut data);
+
+        DataItem { data, path: 0 }
+    }
+
+    pub fn data(&self) -> &Vec<u8> {
+        &self.data
+    }
+
+    pub fn set_data(&mut self, data: Vec<u8>) {
+        self.data = data;
+    }
+
+    pub fn path(&self) -> u16 {
+        self.path
+    }
+
+    pub fn set_path(&mut self, path: u16) {
+        self.path = path;
     }
 }
