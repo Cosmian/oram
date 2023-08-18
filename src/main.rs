30b9f15cb38452c81d57285effe9341c81a5ea4a
Martin Grenouilloux <martin.grenouilloux@cosmian.com>
2023-08-18 12:36:14 +0200

    [fix] Removed old tests from main

--- src/main.rs
+++ src/main.rs
@@ -129,26 +129,3 @@ fn main() -> Result<(), Error> {
 
     Ok(())
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::oram::{Stash, ORAM};
-    use crate::STASH_SIZE;
-
-    #[test]
-    fn complete_tree_test_values() {
-        let stash = Stash::new(STASH_SIZE);
-        let nb_blocks = 129;
-        let block_size = 64;
-        let path_oram = ORAM::new(stash.clone(), nb_blocks, block_size);
-        println!("Hello Path-ORAM!");
-
-        let path = 49;
-        let path_values = path_oram.read_path(path);
-
-        let mut expected_path_values = vec![0, 1, 2, 3, 4, 5, 6, 7];
-        expected_path_values.extend_from_slice(stash.stash());
-
-        assert_eq!(path_values, expected_path_values);
-    }
-}
